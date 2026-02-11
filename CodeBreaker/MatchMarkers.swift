33740697d8fdf928776a761d23223b012a6520bd
evans-prince <princeevans2004@gmail.com>
2026-02-12 00:05:07 +0530

    Initialise CodeBreaker

--- CodeBreaker/MatchMarkers.swift
+++ CodeBreaker/MatchMarkers.swift
@@ -1,14 +1,46 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by PRINCE  on 2/11/26.
+//
+
+import SwiftUI
+
+enum Match{
+    case nomatch
+    case exact
+    case inexact
+}
+
+
 struct MatchMarkers: View {
+    var matches: [Match]
     var body: some View {
         HStack {
             VStack{
-                Circle()
-                Circle()
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
             }
             VStack{
-                Circle()
-                Circle()
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
             }
         }
     }
+    
+    func matchMarker(peg: Int)-> some View{
+        let exactCount: Int = matches.count(where : {match in match == .exact})
+        let foundCount: Int = matches.count(where : {match in match != .nomatch})
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2 )
+            .aspectRatio(1, contentMode: .fit)
+            
+    }
+}
+
+
+#Preview {
+    MatchMarkers(matches:[.exact, .inexact, .nomatch])
 }
