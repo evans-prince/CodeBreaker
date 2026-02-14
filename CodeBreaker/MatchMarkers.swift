29e4a744933124aae6153d2358000ebbcf0a9f8a
evans-prince <princeevans2004@gmail.com>
2026-02-14 16:37:57 +0530

    Assignment 1 completed

--- CodeBreaker/MatchMarkers.swift
+++ CodeBreaker/MatchMarkers.swift
@@ -16,22 +16,24 @@ enum Match{
 
 struct MatchMarkers: View {
     var matches: [Match]
+    //        let exactCount: Int = matches.count(where : {match in match == .exact})
+    var exactCount: Int { matches.count{$0 == .exact}}
+    var foundCount: Int { matches.count{$0 != .nomatch}}
     var body: some View {
         HStack {
-            VStack{
-                matchMarker(peg: 0)
-                matchMarker(peg: 1)
-            }
-            VStack{
-                matchMarker(peg: 2)
-                matchMarker(peg: 3)
+            ForEach(Array(stride(from: 0, to: foundCount, by: 2)), id: \.self) { index in
+                VStack{
+                    matchMarker(peg: index)
+                    if(index+1 < 6) {
+                        matchMarker(peg: index+1)
+                    }
+                }
             }
         }
     }
     
+        // we can skip return if no let or we can add @viewbuilder
     func matchMarker(peg: Int)-> some View{
-        let exactCount: Int = matches.count(where : {match in match == .exact})
-        let foundCount: Int = matches.count(where : {match in match != .nomatch})
         return Circle()
             .fill(exactCount > peg ? Color.primary : Color.clear)
             .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2 )
@@ -41,6 +43,35 @@ struct MatchMarkers: View {
 }
 
 
+struct MatchMarkersPreview: View {
+    let matchesArr : [Match]
+    var body : some View {
+        HStack {
+            ForEach (0..<matchesArr.count, id: \.self) {_ in
+                Circle()
+                    .foregroundStyle(.primary)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            MatchMarkers(matches: matchesArr)
+            Spacer()
+        }
+        .frame(height:45)
+        .padding()
+    }
+}
+
+
 #Preview {
-    MatchMarkers(matches:[.exact, .inexact, .nomatch])
+    VStack {
+        MatchMarkersPreview(matchesArr: [.exact, .inexact , .inexact])
+        MatchMarkersPreview(matchesArr: [ .exact , .nomatch, .nomatch])
+        MatchMarkersPreview(matchesArr: [.exact , .inexact , .inexact, .exact])
+        MatchMarkersPreview(matchesArr: [.exact , .nomatch, .inexact, .inexact])
+        MatchMarkersPreview(matchesArr: [.exact , .nomatch, .nomatch, .inexact])
+        MatchMarkersPreview(matchesArr: [.exact , .exact, .exact, .inexact, .nomatch, .nomatch])
+        MatchMarkersPreview(matchesArr: [.exact , .exact, .exact, .inexact, .inexact, .inexact])
+        MatchMarkersPreview(matchesArr: [.inexact , .exact, .exact, .inexact, .inexact])
+        MatchMarkersPreview(matchesArr: [.exact, .nomatch, .inexact, .nomatch, .inexact])
+    }
+    .padding()
 }
